f19697c45812c80e6fa64b4336a4b864f316a7fb
Quan Mai <20136769@tafe.wa.edu.au>
2026-09-18 11:45:29 +0800

    feat: add BREAD to SQLDataSource

--- lib/services/SQLDataSource.dart
+++ lib/services/SQLDataSource.dart
@@ -24,11 +24,50 @@ class SQLDataSource implements IDataSource {
     return dataSource;
   }
 
+  //BREAD
   @override
   Future<List<Todo>> browse() async {
     List<Map<String, dynamic>> maps = await _database.query('todos');
     return List.generate(maps.length, (index) {
       return Todo.fromMap(maps[index]);
     });
+  }
+
+  @override
+  Future<Todo?> read(String id) async {
+    List<Map<String, dynamic>> maps = await _database.query(
+      'todos',
+      where: 'id = ?',
+      whereArgs: [id],
+      limit: 1,
+    );
+    Todo mapToRead = Todo.fromMap(maps.first);
+    return mapToRead;
+  }
+
+  @override
+  Future<bool> edit(Todo model) async {
+    Map<String, dynamic> map = model.toMap();
+    await _database.update(
+      'todos',
+      map,
+      where: 'id = ?',
+      whereArgs: [model.id],
+    );
+    return true;
+  }
+
+  @override
+  Future<bool> add(Todo model) async {
+    Map<String, dynamic> mapToInsert = model.toMap().remove('id');
+    await _database.insert('todos', mapToInsert);
+    return true;
+  }
+
+  @override
+  Future<bool> delete(Todo model) async {
+    String? id = model.id;
+    await _database.delete('todos', where: 'id = ?', whereArgs: [id]);
+    return true;
   }
 }
